f738f631b5a12a37c7999beebde5191ccbd21be2
motrada <motrada@gmail.com>
2020-02-10 16:07:18 +0100

    testing

---
+++ main.js
@@ -0,0 +1,11 @@
+var a = document.getElementById('a');
+a.click = (event) => {
+    event.preventDefault();
+}
+
+/*
+a.addEventListener('click',(event) => {
+    // code
+    console.log('link clicked')
+})
+*/
